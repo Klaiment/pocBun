31be3b2d5912896dcb06a4deea67d7e67a999bf9
Klaiment <clement62ing@gmail.com>
2024-12-18 09:29:54 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import { Elysia } from "elysia";
+import { swagger } from '@elysiajs/swagger'
+const app = new Elysia();
+let tasks = [];
+app.use(swagger())
+app.get("/", () => "Hello Elysia");
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+app.post("/add-task", ({ body }) => {
+    const { task } = body || {};
+    if (!task) {
+        return {
+            status: 400,
+            body: "Task is required"
+        };
+    }
+    tasks.push(task);
+    return `Task '${task}' added`;
+});
 
+
+
+app.listen(3000)
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
